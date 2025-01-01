4bf6f1dbaf5496eb1debb44b1c789db7a8493827
Erallie <31375589+Erallie@users.noreply.github.com>
2025-01-01 11:35:43 -0700

    Attempting to fix packing and certification
    Not working

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 #Requires -RunAsAdministrator
 $Installer = New-TemporaryFile
 Invoke-WebRequest -Uri https://github.com/Erallie/xs-notify/releases/download/latest/XSNotify.msix -OutFile $Installer
-# Import-Certificate -FilePath $Cert -Cert Cert:\CurrentUser\Root
+# Import-Certificate -FilePath $Cert -Cert Cert:\CurrentUser\My
 Add-AppPackage -AllowUnsigned -Path $Installer
 Start-Process "shell:AppsFolder\$((Get-StartApps | Where-Object {$_.Name -eq 'XSNotify'}).'AppID')"
--- pack.ps1
+++ pack.ps1
@@ -1,4 +1,4 @@
 Copy-Item target/debug/XSNotify.exe ./windows-packaging
 Set-Location windows-packaging
 & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\makeappx.exe" pack /d . /p ../out/XSNotify.msix /nv /o
-& "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /a /v /fd SHA256 /f ../../common/mycert.pfx /p qwertyuiop ../out/XSNotify.msix
+# & "C:\Program Files (x86)\Windows Kits\10\bin\10.0.26100.0\x64\signtool.exe" sign /a /v /fd SHA256 /f C:\Windows\System32\Gozar_Productions_Dev\XSNotify_certificate.pfx /p TryingToDoAThing n/ "CN=XSNotify" ../out/XSNotify.msix
