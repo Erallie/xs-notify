edd3689a1356d99943f4457a2d9e6f7fe42ba882
Erallie <erika@gozarproductions.com>
2025-01-16 12:45:25 -0700

    Changed settings to be children instead of props

--- src/routes/(main)/settings/+page.svelte
+++ src/routes/(main)/settings/+page.svelte
@@ -4,6 +4,11 @@
     import SettingSection from "../../SettingSection.svelte";
 
     let { data } = $props();
+
+    let showEnabled = $state(data.settings.dynamicTimeout);
+    function toggleDynamicSetting(value: boolean) {
+        showEnabled = value;
+    }
 </script>
 
 <section class="container">
@@ -18,121 +23,102 @@
             <SettingSection
                 title="General"
                 description="General settings for XS Notify."
-                settings={[
-                    {
-                        title: "Auto-run",
-                        description:
-                            "Runs the notification bridge on application launch.",
-                        settings: settings,
-                        setting: WhichSetting.autoRun,
-                    },
-                    {
-                        title: "Polling Rate",
-                        description:
-                            "The rate at which XS Notify checks for new notifications, specified in milliseconds.",
-                        settings: settings,
-                        setting: WhichSetting.pollingRate,
-                    },
-                    {
-                        title: "Skipped apps",
-                        description:
-                            "A list of names of apps that XS Notify will not push notifications for.",
-                        settings: settings,
-                        setting: WhichSetting.skippedApps,
-                    },
-                ]}
-            />
+                ><Setting
+                    title="Auto-run"
+                    description="Runs the notification bridge on application launch."
+                    {settings}
+                    setting={WhichSetting.autoRun}
+                /><Setting
+                    title="Polling Rate"
+                    description="The rate at which XS Notify checks for new notifications, specified in milliseconds."
+                    {settings}
+                    setting={WhichSetting.pollingRate}
+                /><Setting
+                    title="Skipped apps"
+                    description="A list of names of apps that XS Notify will not push notifications for."
+                    {settings}
+                    setting={WhichSetting.skippedApps}
+                /></SettingSection
+            >
             <!-- Dynamic Notifications Settings -->
             <SettingSection
                 title="Dynamic Notifications"
                 description="Dynamically sets the notification display time based on the length of the notification text."
-                settings={{
-                    mainSetting: {
-                        title: "Enable Dynamic Notifications",
-                        settings: settings,
-                        setting: WhichSetting.dynamicTimeout,
-                    },
-                    enabledSettings: [
-                        {
-                            title: "Reading Speed",
-                            description:
-                                "Your reading speed in words per minute (WPM), which will be used to calculate the display time for dynamic notifications.",
-                            settings: settings,
-                            setting: WhichSetting.readingSpeed,
-                        },
-                        {
-                            title: "Minimum notification time",
-                            description:
-                                "The minimum amount of time a notification will be displayed, in seconds.",
-                            settings: settings,
-                            setting: WhichSetting.minTimeout,
-                        },
-                        {
-                            title: "Maximum notification time",
-                            description:
-                                "The maximum amount of time a notification will be displayed, in seconds.",
-                            settings: settings,
-                            setting: WhichSetting.maxTimeout,
-                        },
-                    ],
-                    disabledSettings: [
-                        {
-                            title: "Notification time",
-                            description:
-                                "The amount of time for which a notification will be displayed, in seconds",
-                            settings: settings,
-                            setting: WhichSetting.defaultTimeout,
-                        },
-                    ],
-                }}
-            />
+            >
+                <Setting
+                    title="Enable Dynamic Notifications"
+                    {settings}
+                    setting={WhichSetting.dynamicTimeout}
+                    callback={toggleDynamicSetting}
+                />
+                {#if showEnabled}
+                    <Setting
+                        title="Reading Speed"
+                        description="Your reading speed in words per minute (WPM), which will be used to calculate the display time for dynamic notifications."
+                        {settings}
+                        setting={WhichSetting.readingSpeed}
+                    />
+                    <Setting
+                        title="Minimum notification time"
+                        description="The minimum amount of time a notification will be displayed, in seconds."
+                        {settings}
+                        setting={WhichSetting.minTimeout}
+                    />
+                    <Setting
+                        title="Maximum notification time"
+                        description="The maximum amount of time a notification will be displayed, in seconds."
+                        {settings}
+                        setting={WhichSetting.maxTimeout}
+                    />
+                {:else if !showEnabled}
+                    <Setting
+                        title="Notification time"
+                        description="The amount of time for which a notification will be displayed, in seconds"
+                        {settings}
+                        setting={WhichSetting.defaultTimeout}
+                    />
+                {/if}
+            </SettingSection>
 
             <!-- XSOverlay Settings -->
-            <SettingSection
-                title="XSOverlay"
-                settings={[
-                    {
-                        title: "Port",
-                        description: "The port that XSOverlay is listening on.",
-                        settings: settings,
-                        setting: WhichSetting.port,
-                    },
-                    {
-                        title: "Host",
-                        description:
-                            "The hostname that XSOverlay is listening on.",
-                        settings: settings,
-                        setting: WhichSetting.host,
-                    },
-                ]}
-            />
+            <SettingSection title="XSOverlay">
+                <Setting
+                    title="Port"
+                    description="The port that XSOverlay is listening on."
+                    {settings}
+                    setting={WhichSetting.port}
+                />
+                <Setting
+                    title="Host"
+                    description="The hostname that XSOverlay is listening on."
+                    {settings}
+                    setting={WhichSetting.host}
+                />
+            </SettingSection>
 
             <SettingSection
                 title="Startup"
                 description="Options relating to the startup of XS Notify."
-                settings={[
-                    {
-                        title: "Auto-launch",
-                        description: "Launch XS Notify on system startup.",
-                        settings: settings,
-                        setting: WhichSetting.autoLaunch,
-                    },
-                    {
-                        title: "Minimize to Tray",
-                        description:
-                            "Minimize to the system tray instead of closing.",
-                        settings: settings,
-                        setting: WhichSetting.minimize,
-                    },
-                    {
-                        title: "Start Minimized",
-                        description:
-                            "Minimize XS Notify to the system tray when it launches.",
-                        settings: settings,
-                        setting: WhichSetting.minimizeOnStart,
-                    },
-                ]}
-            />
+            >
+                <Setting
+                    title="Auto-launch"
+                    description="Launch XS Notify on system startup."
+                    {settings}
+                    setting={WhichSetting.autoLaunch}
+                />
+                <Setting
+                    title="Minimize to Tray"
+                    description="Minimize to the system tray instead of closing."
+                    {settings}
+                    setting={WhichSetting.minimize}
+                />
+                <Setting
+                    title="Start Minimized"
+                    description="Minimize XS Notify to the system tray when it launches."
+                    {settings}
+                    setting={WhichSetting.minimizeOnStart}
+                />
+            </SettingSection>
         </div>
     {/await}
 </section>
--- src/routes/SettingSection.svelte
+++ src/routes/SettingSection.svelte
@@ -1,24 +1,7 @@
 <script lang="ts">
-    import {
-        type SettingSection,
-        isDynamicSettings,
-        isSetting,
-    } from "$lib/types/types";
-    import Setting from "./Setting.svelte";
+    import { type SettingSection } from "$lib/types/types";
 
-    let { title, description, settings }: SettingSection = $props();
-
-    if (isDynamicSettings(settings)) {
-    }
-    function setDynamicSetting() {
-        if (isDynamicSettings(settings)) {
-            return settings.mainSetting.settings.dynamicTimeout;
-        }
-    }
-    let showEnabled = $state(setDynamicSetting());
-    function toggleDynamicSetting(value: boolean) {
-        showEnabled = value;
-    }
+    let { title, description, children }: SettingSection = $props();
 </script>
 
 <div class="my-12">
@@ -29,48 +12,6 @@
         </p>
     {/if}
     <div class="py-4 space-y-8">
-        {#if Array.isArray(settings)}
-            {#each settings as setting}
-                {#if isSetting(setting)}
-                    <Setting
-                        title={setting.title}
-                        description={setting.description}
-                        settings={setting.settings}
-                        setting={setting.setting}
-                    />
-                {:else}
-                    <p class="text-error">
-                        <code>{settings}</code> is not a valid setting!
-                    </p>
-                {/if}
-            {/each}
-        {:else if isDynamicSettings(settings)}
-            <Setting
-                title={settings.mainSetting.title}
-                description={settings.mainSetting.description}
-                settings={settings.mainSetting.settings}
-                setting={settings.mainSetting.setting}
-                callback={toggleDynamicSetting}
-            />
-            {#if showEnabled}
-                {#each settings.enabledSettings as setting}
-                    <Setting
-                        title={setting.title}
-                        description={setting.description}
-                        settings={setting.settings}
-                        setting={setting.setting}
-                    />
-                {/each}
-            {:else if !showEnabled}
-                {#each settings.disabledSettings as setting}
-                    <Setting
-                        title={setting.title}
-                        description={setting.description}
-                        settings={setting.settings}
-                        setting={setting.setting}
-                    />
-                {/each}
-            {/if}
-        {/if}
+        {@render children()}
     </div>
 </div>
