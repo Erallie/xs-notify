eaf938f0afe79868e49473324a25d7533e5c841e
Zelaf <10819462+Zelaf@users.noreply.github.com>
2025-01-16 19:08:34 +0100

    New settings page

--- src/routes/(main)/settings/+page.svelte
+++ src/routes/(main)/settings/+page.svelte
@@ -1,138 +1,148 @@
 <script lang="ts">
-    import { WhichSetting } from "$lib/types/types";
-    import Setting from "../../Setting.svelte";
-    import SettingSection from "../../SettingSection.svelte";
+    import MinMaxSlider from "$lib/components/settings/MinMaxSlider.svelte";
+    import MultiSelect from "$lib/components/settings/MultiSelect.svelte";
+    import NumberInputBig from "$lib/components/settings/NumberInputBig.svelte";
+    import SettingSection from "$lib/components/settings/SettingSection.svelte";
+    import Switch from "$lib/components/settings/Switch.svelte";
+    import { fly, slide } from "svelte/transition";
 
     let { data } = $props();
+
+    let newSettings = $state(data.settings);
+
+    let rangeSlider: number[] = $state([
+        newSettings.minTimeout,
+        newSettings.maxTimeout,
+    ]);
+
+    let optionsChanged: boolean = $derived(
+        JSON.stringify(newSettings) !== JSON.stringify(data.settings),
+    );
+
+    $effect(() => {
+        newSettings.minTimeout = rangeSlider[0];
+        newSettings.maxTimeout = rangeSlider[1];
+    });
 </script>
 
-<section class="container">
+<div class="container">
     <h1 class="text-center text-4xl font-extrabold text-nowrap tracking-wide">
         Settings
     </h1>
-    {#await data.settings}
-        loading settings... <!-- replace with something to display while settings are loading -->
-    {:then settings}
-        <div class="my-12">
-            <!-- General Settings -->
-            <SettingSection
-                title="General"
-                description="General settings for XS Notify."
-                settings={[
-                    {
-                        title: "Auto-run",
-                        description:
-                            "Runs the notification bridge on application launch.",
-                        settings: settings,
-                        setting: WhichSetting.autoRun,
-                    },
-                    {
-                        title: "Polling Rate",
-                        description:
-                            "The rate at which XS Notify checks for new notifications, specified in milliseconds.",
-                        settings: settings,
-                        setting: WhichSetting.pollingRate,
-                    },
-                    {
-                        title: "Skipped apps",
-                        description:
-                            "A list of names of apps that XS Notify will not push notifications for.",
-                        settings: settings,
-                        setting: WhichSetting.skippedApps,
-                    },
-                ]}
+    <div class="my-12 space-y-16">
+        <!-- General Settings -->
+        <SettingSection
+            title="General"
+            description="General settings for XSNotify"
+        >
+            <Switch
+                bind:checked={newSettings.autoRun}
+                class="toggle-primary"
+                label="Run on boot"
+                description="Runs XSNotify on boot."
             />
-            <!-- Dynamic Notifications Settings -->
-            <SettingSection
-                title="Dynamic Notifications"
-                description="Dynamically sets the notification display time based on the length of the notification text."
-                settings={{
-                    mainSetting: {
-                        title: "Enable Dynamic Notifications",
-                        settings: settings,
-                        setting: WhichSetting.dynamicTimeout,
-                    },
-                    enabledSettings: [
-                        {
-                            title: "Reading Speed",
-                            description:
-                                "Your reading speed in words per minute (WPM), which will be used to calculate the display time for dynamic notifications.",
-                            settings: settings,
-                            setting: WhichSetting.readingSpeed,
-                        },
-                        {
-                            title: "Minimum notification time",
-                            description:
-                                "The minimum amount of time a notification will be displayed, in seconds.",
-                            settings: settings,
-                            setting: WhichSetting.minTimeout,
-                        },
-                        {
-                            title: "Maximum notification time",
-                            description:
-                                "The maximum amount of time a notification will be displayed, in seconds.",
-                            settings: settings,
-                            setting: WhichSetting.maxTimeout,
-                        },
-                    ],
-                    disabledSettings: [
-                        {
-                            title: "Notification time",
-                            description:
-                                "The amount of time for which a notification will be displayed, in seconds",
-                            settings: settings,
-                            setting: WhichSetting.defaultTimeout,
-                        },
-                    ],
+            <NumberInputBig
+                variant="primary"
+                label="Polling rate"
+                description="The rate which XSNotify will check for new notifications."
+                bind:value={newSettings.pollingRate}
+            />
+            <MultiSelect bind:selected={newSettings.skippedApps} />
+        </SettingSection>
+
+        <!-- Dynamic Notifications Settings -->
+        <SettingSection
+            title="Notifications"
+            description="Notification options for XSNotify"
+        >
+            <Switch
+                label="Enable Dynamic Notifications"
+                class="toggle-primary"
+                bind:checked={newSettings.dynamicTimeout}
+            />
+            {#if newSettings.dynamicTimeout}
+                <div
+                    transition:slide
+                    class="space-y-8 card card-bordered card-compact shadow-xl bg-neutral/40 text-neutral-content"
+                >
+                    <div class="card-body">
+                        <NumberInputBig
+                            variant="primary"
+                            label="Reading speed"
+                            description="Your reading speed in WPM (Words per minute) which will be used to calculate the time notifications will be shown for."
+                            bind:value={newSettings.readingSpeed}
+                        />
+                        <div>
+                            <MinMaxSlider
+                                label="Notification time"
+                                min={2}
+                                max={300}
+                                bind:values={rangeSlider}
+                            />
+                            <div class="flex flex-row justify-between label">
+                                <span class="label-text text-pretty"
+                                    >Minimum</span
+                                >
+                                <span class="label-text text-pretty"
+                                    >Maximum</span
+                                >
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            {:else}
+                <div transition:slide>
+                    <NumberInputBig
+                        variant="primary"
+                        label="Notification timeout"
+                        description="The amount of time a notification will be shown."
+                        bind:value={newSettings.defaultTimeout}
+                    />
+                </div>
+            {/if}
+        </SettingSection>
+
+        <!-- XSOverlay Settings -->
+        <SettingSection
+            title="XSOverlay"
+            description="Settings for XSNotify to connect to XSOverlay"
+        >
+            <div>
+                <div class="text-lg font-semibold mb-2">Hostname</div>
+                <input
+                    bind:value={newSettings.host}
+                    class="input input-primary"
+                />
+                <div class="label">
+                    <span class="label-text text-pretty">
+                        The IP or hostname that XSOverlay is running on.
+                    </span>
+                </div>
+            </div>
+            <NumberInputBig
+                variant="primary"
+                label="Port"
+                description="The port XSOverlay is accessible on."
+                bind:value={newSettings.port}
+            />
+        </SettingSection>
+    </div>
+</div>
+
+<!-- Alert of changed options -->
+{#if optionsChanged}
+    <div out:fly={{ y: 20 }} class="toast toast-center mb-16 z-10">
+        <div class="alert alert-info gap-6">
+            <span
+                >You need to restart the notification server to apply the new
+                settings.</span
+            >
+            <button
+                onclick={() => {
+                    console.log("Restart action here.");
                 }}
-            />
-
-            <!-- XSOverlay Settings -->
-            <SettingSection
-                title="XSOverlay"
-                settings={[
-                    {
-                        title: "Port",
-                        description: "The port that XSOverlay is listening on.",
-                        settings: settings,
-                        setting: WhichSetting.port,
-                    },
-                    {
-                        title: "Host",
-                        description:
-                            "The hostname that XSOverlay is listening on.",
-                        settings: settings,
-                        setting: WhichSetting.host,
-                    },
-                ]}
-            />
-
-            <SettingSection
-                title="Startup"
-                description="Options relating to the startup of XS Notify."
-                settings={[
-                    {
-                        title: "Auto-launch",
-                        description: "Launch XS Notify on system startup.",
-                        settings: settings,
-                        setting: WhichSetting.autoLaunch,
-                    },
-                    {
-                        title: "Minimize to Tray",
-                        description:
-                            "Minimize to the system tray instead of closing.",
-                        settings: settings,
-                        setting: WhichSetting.minimize,
-                    },
-                    {
-                        title: "Start Minimized",
-                        description:
-                            "Minimize XS Notify to the system tray when it launches.",
-                        settings: settings,
-                        setting: WhichSetting.minimizeOnStart,
-                    },
-                ]}
-            />
+                class="btn btn-secondary btn-sm h-10">Apply & Restart</button
+            >
         </div>
-    {/await}
-</section>
+    </div>
+{/if}
