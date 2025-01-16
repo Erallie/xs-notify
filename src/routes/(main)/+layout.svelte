03f134e96c9c36ca2d79e46769f67a6a10db2c65
Zelaf <10819462+Zelaf@users.noreply.github.com>
2025-01-16 19:06:15 +0100

    New layouting & move client settings to top layout.

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,148 +1,86 @@
 <script lang="ts">
-    import "../../app.pcss";
-    import { invoke } from "@tauri-apps/api/core";
-    import { listen } from "@tauri-apps/api/event";
-    import { setContext } from "svelte";
-    import type { Logs } from "$lib/types/types.js";
-    import { attachLogger } from "@tauri-apps/plugin-log";
-
-    let { children, data } = $props();
-
-    let isRunning = $state(data.isRunning);
-
-    let toggleRunText = $state(setRunText());
-
-    function setRunText() {
-        switch (isRunning) {
-            case true:
-                return "Stop Bridge";
-            case false:
-                return "Start Bridge";
-        }
-    }
-
-    async function toggleRun() {
-        isRunning = await invoke<boolean>("toggle_run");
-        toggleRunText = setRunText();
-    }
-
-    listen<boolean>("toggle-bridge", (event) => {
-        isRunning = event.payload;
-        toggleRunText = setRunText();
-        // console.error("testing");
-        // console.log(`isRunning = ${isRunning}`);
-    });
-
-    // with TargetKind::Webview enabled this function will print logs to the browser console
-
-    let detachLogger: () => void;
-    // Use $effect to handle side effects
-    $effect(() => {
-        // Create an async function to handle the logger attachment
-        const attachLoggerAsync = async () => {
-            detachLogger = await attachLogger((value) => {
-                addToConsole(value.message, value.level);
-            });
-        };
-
-        // Call the async function
-        attachLoggerAsync();
-
-        // Cleanup function to detach the logger
-        return () => {
-            if (detachLogger) {
-                detachLogger();
-            }
-        };
-    });
-
-    let logElement: Logs[] = $state([]);
-    setContext("logElement", logElement);
-
-    function addToConsole(message: string, level: number) {
-        let consoleEl = document.getElementById("console-el");
-
-        let isAtBottom = false;
-        if (
-            consoleEl &&
-            consoleEl.scrollTop >=
-                consoleEl.scrollHeight - consoleEl.clientHeight * 1.5
-        ) {
-            isAtBottom = true;
-        }
-        let cls = "text-gray-500";
-        let extraClasses;
-        switch (level) {
-            case 5:
-                cls = "text-red-900";
-                extraClasses = "bg-warning text-warning-content";
-                break;
-            case 4:
-                cls = "text-orange-900";
-                extraClasses = "bg-orange-400 text-warning-content";
-                break;
-        }
-        let newMsg = message.replace(
-            /^(\[.+\]) (.+)$/gm,
-            `<span class="${cls}">$1</span><br>$2`,
-        );
-        logElement.push({
-            msg: newMsg,
-            level: level,
-            extraClasses: extraClasses,
-        });
-        if (isAtBottom) {
-            consoleEl!.scrollTop = consoleEl!.scrollHeight;
-        }
-    }
+    import { page } from "$app/state";
+    import Toaster from "$lib/components/toaster/Toaster.svelte";
+    let { children } = $props();
 </script>
 
-<main class="drawer lg:drawer-open">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content flex flex-col items-center justify-center my-12">
-        <!-- Page content here -->
+<Toaster />
 
-        <label
-            for="my-drawer"
-            class="btn drawer-button lg:hidden fixed top-6 left-8 btn-neutral"
-        >
-            <svg
-                height="21"
-                width="21"
-                viewBox="0 0 21 21"
-                xmlns="http://www.w3.org/2000/svg"
-            >
-                <g
-                    stroke="currentColor"
-                    stroke-width="4"
-                    stroke-linecap="round"
-                >
-                    <line x1="2" y1="10" x2="19" y2="10" />
-                    <line x1="2" y1="2" x2="19" y2="2" />
-                    <line x1="2" y1="19" x2="19" y2="19" />
-                </g>
-                =
-            </svg>
-        </label>
+<main>
+    <div class="container py-12 mb-16">
         {@render children()}
     </div>
-    <div class="drawer-side">
-        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"
-        ></label>
-        <nav class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-            <!-- Sidebar content here -->
-            <ul>
-                <li><a href="/">About</a></li>
-                <li>
-                    <a href="/settings" aria-label="close sidebar">Settings</a>
-                </li>
-                <li>
-                    <a href="/console">Console</a>
-                </li>
-                <li>
-                    <button onclick={toggleRun}>{toggleRunText}</button>
-                </li>
-            </ul>
-        </nav>
+
+    <!-- Bottom Navigation -->
+    <div class="btm-nav z-10">
+        <a
+            href="/console"
+            class="transition-all {page.url.pathname === '/console'
+                ? 'active'
+                : ''}"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z"
+                />
+            </svg>
+            <span class="btm-nav-label">Console</span>
+        </a>
+        <a
+            href="/settings"
+            class="transition-all {page.url.pathname === '/settings'
+                ? 'active'
+                : ''}"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.325.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 0 1 1.37.49l1.296 2.247a1.125 1.125 0 0 1-.26 1.431l-1.003.827c-.293.241-.438.613-.43.992a7.723 7.723 0 0 1 0 .255c-.008.378.137.75.43.991l1.004.827c.424.35.534.955.26 1.43l-1.298 2.247a1.125 1.125 0 0 1-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.47 6.47 0 0 1-.22.128c-.331.183-.581.495-.644.869l-.213 1.281c-.09.543-.56.94-1.11.94h-2.594c-.55 0-1.019-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 0 1-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 0 1-1.369-.49l-1.297-2.247a1.125 1.125 0 0 1 .26-1.431l1.004-.827c.292-.24.437-.613.43-.991a6.932 6.932 0 0 1 0-.255c.007-.38-.138-.751-.43-.992l-1.004-.827a1.125 1.125 0 0 1-.26-1.43l1.297-2.247a1.125 1.125 0 0 1 1.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.086.22-.128.332-.183.582-.495.644-.869l.214-1.28Z"
+                />
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                />
+            </svg>
+            <span class="btm-nav-label">Settings</span>
+        </a>
+        <a
+            href="/"
+            class="transition-all {page.url.pathname === '/' ? 'active' : ''}"
+        >
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="size-6"
+            >
+                <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+                />
+            </svg>
+            <span class="btm-nav-label">About</span>
+        </a>
     </div>
 </main>
