ec6ee439b467ed4dbddbf2b857d2636e86170a22
Erallie <erika@gozarproductions.com>
2025-01-20 09:18:30 -0700

    Made the console-el dynamically resize properly

--- src/routes/(main)/console/+page.svelte
+++ src/routes/(main)/console/+page.svelte
@@ -80,7 +80,7 @@
     }
 </script>
 
-<section class="container">
+<section id="console-container" class="container">
     <h1
         class="text-center text-4xl font-extrabold text-nowrap tracking-wide mb-12"
     >
