6d1f0591385bba2f5ee5d756193b758d9b1a8255
Erallie <erika@gozarproductions.com>
2025-01-21 08:31:26 -0700

    removed unnecessary types

--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -20,48 +20,9 @@ export type XSNotifySettings = {
     minimizeOnStart: boolean;
 }
 
-export enum SettingType {
-    toggle,
-    string,
-    number,
-    stringArray
-}
-
-export type Setting = {
-    title: string;
-    description?: string;
-    warning?: boolean;
-    settings: XSNotifySettings;
-    setting: WhichSetting;
-    callback?: Function;
-}
-
-export enum WhichSetting {
-    autoRun,
-
-    port,
-    host,
-    pollingRate,
-
-    dynamicTimeout,
-    defaultTimeout,
-
-    readingSpeed,
-    minTimeout,
-    maxTimeout,
-
-    isWhitelist,
-    appList,
-
-    autoLaunch,
-    minimize,
-    minimizeOnStart,
-}
-
 export type SettingSection = {
     title: string;
     description?: string;
-    warning?: boolean;
     children: any;
 }
 
